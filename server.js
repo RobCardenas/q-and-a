7f31dfd86db850218b3652b354281085ea1a2a56
Rob <robbcardenas@gmail.com>
2015-08-31 17:52:39 -0700

    api routes working

---
+++ server.js
@@ -0,0 +1,148 @@
+// require express and other modules
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),  // for data from the request body
+    mongoose = require('mongoose'),
+    Question = require('./models/question'),
+    Answer = require('./models/answer');       // to interact with our db
+
+// connect to mongodb
+mongoose.connect(
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/q_and_a'
+);
+
+// configure body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+// route (index.html)
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/views/index.html');
+});
+
+// send back all questions
+app.get('/api/questions', function (req, res) {
+  Question.find({}, function (err, questions) {
+    res.json(questions);
+  });
+});
+
+// create new question
+app.post('/api/questions', function (req, res) {
+  // create new question with data from the body of the request (`req.body`)
+  // body should contain the question text itself
+  var newQuestion = new Question({
+    text: req.body.text
+  });
+
+  // save new question
+  newQuestion.save(function (err, savedQuestion) {
+    res.json(savedQuestion);
+  });
+});
+
+// route for single id
+app.get('/api/questions/:id', function (req, res) {
+	//set the value of the id
+    var targetId = req.params.id;
+    //find correct question in the db by id
+    Question.findOne({_id: targetId}, function (err, foundQuestion) {
+      res.json(foundQuestion);
+    });
+  });
+
+// update question, but only the part(s) passed in in the request body
+// not currently that exciting when question has only one attribute
+app.put('/api/questions/:id', function (req, res) {
+  // set the value of the id
+  var targetId = req.params.id;
+
+  // find question in db by id
+  Question.findOne({_id: targetId}, function (err, foundQuestion) {
+    // update the question's text, if the new text passed in was truthy
+    // otherwise keep the same text
+    foundQuestion.text = req.body.text || foundQuestion.text;
+
+    // save updated question in db
+    foundQuestion.save(function (err, savedQuestion) {
+      res.json(savedQuestion);
+    });
+  });
+});
+
+// route to delete a specific question
+app.delete('/api/questions/:id', function (req, res) {
+    //set the value of the desired id
+    var targetId = req.params.id;
+    //find the correct post in the db and remove it
+    Question.findOneAndRemove({_id: targetId}, function (err, deletedQuestion) {
+      res.json(deletedQuestion);
+    });
+  });
+
+// send back all answers
+// app.get('/api/questions/:questionId/answers', function (req, res) {
+//   Answer.find({}, function (err, answers) {
+//     res.json(answers);
+//   });
+// });
+
+// create new answer
+app.post('/api/questions/:questionId/answers', function (req, res) {
+  // create new question with data from the body of the request (`req.body`)
+  // body should contain the question text itself
+  var questionId = req.params.questionId;
+  var newAnswer = new Answer({
+    content: req.body.content
+  });
+
+  // save new answer
+ Question.findOne({_id: questionId}, function (err, foundQuestion) {
+    foundQuestion.answers.push(newAnswer);
+    foundQuestion.save(function (err, savedQuestion) {
+      res.json(newAnswer);
+    });
+	});
+});
+
+// update answer embedded in question
+app.put('/api/questions/:questionId/answers/:id', function (req, res) {
+  // set the value of the list and todo ids
+  var questionId = req.params.questionId;
+  var answerId = req.params.id;
+
+  // find list in db by id
+  Question.findOne({_id: questionId}, function (err, foundQuestion) {
+    // find todo embedded in list
+    var foundAnswer = foundQuestion.answers.id(answerId);
+    // update answer content and completed with data from request body
+    foundAnswer.content = req.body.content;
+    foundQuestion.save(function (err, savedAnswer) {
+      res.json(foundAnswer);
+    });
+  });
+});
+
+// update answer embedded in question
+app.delete('/api/questions/:questionId/answers/:id', function (req, res) {
+  // set the value of the list and todo ids
+  var questionId = req.params.questionId;
+  var answerId = req.params.id;
+
+  // find list in db by id
+  Question.findOne({_id: questionId}, function (err, foundQuestion) {
+    // find todo embedded in list
+    var foundAnswer = foundQuestion.answers.id(answerId);
+    // update answer content and completed with data from request body
+    foundAnswer.remove();
+    foundQuestion.save(function (err, savedAnswer) {
+      res.json(foundAnswer);
+    });
+  });
+});
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('server started on localhost:3000');
+});
